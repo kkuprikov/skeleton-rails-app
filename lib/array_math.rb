2430b3e72589d806d342efb608bfa757e10b21fe
Kirill Kuprikov <kirill.kuprikov@railsware.com>
2023-05-26 02:55:56 +0700

    Style fix

--- lib/array_math.rb
+++ lib/array_math.rb
@@ -3,11 +3,7 @@
 # Test 3
 module ArrayMath
   def calculate_average(a)
-    if valid_array?(a)
-      a.sum / Float(a.length)
-    else
-      0
-    end
+    valid_array?(a) ? a.sum / Float(a.length) : 0
   end
 
   private
