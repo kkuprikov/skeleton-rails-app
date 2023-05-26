a18a97fb81ae7f03ba3bde43b9f4b53baddc7e4f
Kirill Kuprikov <kirill.kuprikov@railsware.com>
2023-05-26 16:44:59 +0700

    Utilizing Numeric class to check array elements

--- lib/array_math.rb
+++ lib/array_math.rb
@@ -3,12 +3,12 @@
 # Test 3
 module ArrayMath
   def calculate_average(a)
-    valid_array?(a) ? a.sum / Float(a.length) : 0
+    valid_array?(a) ? Float(a.sum) / a.length : 0
   end
 
   private
 
   def valid_array?(a)
-    a.is_a?(Array) && a.length.positive? && a.all? { |el| el.is_a?(Integer) || el.is_a?(Float) }
+    a.is_a?(Array) && a.length.positive? && a.all?(Numeric)
   end
 end
